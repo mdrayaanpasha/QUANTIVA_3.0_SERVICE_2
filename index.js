31f436e21178885804bf6f402118b8bf04c7e7cb
mohammed rayaan pasha <mdrayaanpasha@gmail.com>
2026-03-06 19:29:52 +0530

    added the rabitmq thingy

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 import express from "express";
 import cors from "cors";
+import amqplib from "amqplib";
+import { createClient } from 'redis';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = createClient({
+  url: process.env.REDIS_KEY
+});
+
+const rabbitmqUrl = process.env.RABITMQ_KEY;
+
+async function connectRabbitMQ() {
+  try {
+    const connection = await amqplib.connect(rabbitmqUrl);
+    channel = await connection.createChannel();
+    console.log("Connected to RabbitMQ");
+  } catch (error) {
+    console.error("Failed to connect to RabbitMQ:", error);
+  }
+}
+
+let Queue = "fin_queue";
+
+let conn = await amqplib.connect(rabbitmqUrl);
+const channel = await conn.createChannel();
+
+await channel.assertQueue(Queue, { durable: true });
+await channel.assertExchange("amq.direct", "direct", { durable: true });
+
+await channel.consume(Queue, (msg) => {
+  if (msg !== null) {
+    console.log("Received message:", msg.content.toString());
+    channel.ack(msg);
+  }
+}, { noAck: false }); 
+
+client.on('error', err => console.log('Redis Client Error', err));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
