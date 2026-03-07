eb02c61b0daf1250694e5a4abd42d004b898aa46
mohammed rayaan pasha <mdrayaanpasha@gmail.com>
2026-03-07 12:51:29 +0530

    added this thingy:

--- index.js
+++ index.js
@@ -89,6 +89,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
